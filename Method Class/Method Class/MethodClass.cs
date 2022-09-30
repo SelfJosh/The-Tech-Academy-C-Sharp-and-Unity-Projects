cc8000884966e217c27731649048a26a7666fa4e
SelfJosh <99226546+SelfJosh@users.noreply.github.com>
2022-09-30 16:05:57 -0700

    Method Class Submission Assignment

---
+++ Method Class/Method Class/MethodClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class
+{
+    public class MethodClass
+    {
+        public static void VoidMethod(int x, int y)
+        {
+            Console.WriteLine((x * 15) / 5);
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodClass x = new MethodClass();
+
+            Console.WriteLine("Please input two numbers. The second one will be displayed on the screen as is and the first number will have some math operations done on it.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nHere are the results:");
+            MethodClass.VoidMethod(num1, num2);
+        }
+    }
+}
