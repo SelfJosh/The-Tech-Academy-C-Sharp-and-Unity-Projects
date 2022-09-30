ad1b290d4e1cf84a9e1834b0a11e6df97f0e9288
SelfJosh <99226546+SelfJosh@users.noreply.github.com>
2022-09-30 15:52:18 -0700

    Method Submission Assignment

---
+++ Methods/Methods/DoubleInteger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public class DoubleInteger
+    {
+        public static int DoubleInput(int x, int y = 1)
+        {
+            int result = (x * 15 + 28) * y + (18 / 4 + x) ;
+
+            return result;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleInteger x = new DoubleInteger();
+
+            int num2;
+
+            Console.WriteLine("Please enter two numbers one at a time.\nThe second number is optional.");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Here are the results:" + DoubleInteger.DoubleInput(num1, num2));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Here are the results: " + DoubleInteger.DoubleInput(num1));
+            }
+            Console.ReadLine();
+
+            
+        }
+    }
+}
