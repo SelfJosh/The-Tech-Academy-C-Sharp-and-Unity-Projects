9abcbd7d6d57abce031b74c8a64769ef08ff218a
SelfJosh <99226546+SelfJosh@users.noreply.github.com>
2022-09-30 17:43:46 -0700

    Methods and Objects Submission Assignment

---
+++ Methods and Objects/Methods and Objects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Methods_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
