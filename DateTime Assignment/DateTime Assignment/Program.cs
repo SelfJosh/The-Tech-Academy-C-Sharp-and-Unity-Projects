9749a63a2d652b3b9bf5766ab2d9f366e70dbf28
Josh Becerra <99226546+SelfJosh@users.noreply.github.com>
2023-05-30 15:04:38 -0700

    DateTime Submission Assignment
    Learning how to add values to DateTime

---
+++ DateTime Assignment/DateTime Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTime_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please enter a number to see what time it will be in x amount of hours.");
+
+            double userNumber = Convert.ToInt32(Console.ReadLine());
+
+            DateTime Time = DateTime.Now;
+
+            
+
+            Console.WriteLine("It will be {0}, in {1} hours!", Time.AddHours(userNumber), userNumber);
+            Console.ReadLine();
+        }
+    }
+}
